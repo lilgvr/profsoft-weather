acc1dd6bfae054f6ac98d36797bf48774ce69c73
lilgvr <71455385+lilgvr@users.noreply.github.com>
2022-12-23 21:56:04 +0400

    GetWeather service component

--- src/components/desktop/header/Header.tsx
+++ src/components/desktop/header/Header.tsx
@@ -1,9 +1,7 @@
-import React, { FC, FormEvent, useCallback, useContext, useEffect, useState } from 'react';
+import React, { FC, FormEvent, useState } from 'react';
 import styles from "./header.module.scss";
-import { useActions, useAppSelector } from "../../../hooks";
+import { useAppSelector } from "../../../hooks";
 import { Link, useLocation } from "react-router-dom";
-import { useLazyGetWeekWeatherQuery } from "../../../store/weather";
-import { useLazyGetGeolocationByCoordinatesQuery } from "../../../store/geolocation";
 import { SvgIcon } from "../../common/svg-icon";
 import { GetWeather } from "../../common/get-weather/GetWeather";
 
@@ -16,81 +14,16 @@ export const Header: FC = () => {
 
     const location = useLocation();
 
-    // const { setWeather, geolocationSuccess, geolocationError } = useActions();
-
-    /*const { weather } = useAppSelector(state => state.weather);
-    const { geolocation, isError } = useAppSelector(state => state.geolocation);
-
-    const [trigger, { data, error: weatherError, isSuccess }] = useLazyGetWeekWeatherQuery();
-    const [geoTrigger, {
-        data: geoData,
-        error: geoError,
-        isSuccess: isGeoSuccess,
-    }] = useLazyGetGeolocationByCoordinatesQuery();
-
-    const location = useLocation();
-
-    const getGeolocation = useCallback(() => { // get user geolocation
-        const success: PositionCallback = (position) => {
-            const { latitude, longitude } = position.coords;
-
-            geolocationSuccess([latitude, longitude]);
-        }
-
-        const error: PositionErrorCallback = (error) => {
-            console.log(error.message);
-            geolocationError(null);
-        }
-
-        const options: PositionOptions = {
-            timeout: 5000,
-            maximumAge: Infinity,
-            enableHighAccuracy: true
-        }
-        navigator.geolocation.getCurrentPosition(success, error, options);
-    }, []);
-
-    useEffect(() => {
-        getGeolocation();
-    }, []);
-
-    useEffect(() => { // fetch city name and weather
-        const fetch = async () => {
-            if (geolocation) {
-                await geoTrigger({ lat: geolocation[0], lon: geolocation[1] });
-
-                if (geoData) {
-                    await trigger({ location: geoData });
-                    if (data) setWeather(data)
-                }
-            }
-        }
-
-        fetch()
-    }, [isGeoSuccess, geolocation, isError]);*/
-
-    /*
-    * ================= Handlers =================
-    *
-    * */
-
     const handleInputChange = (e: FormEvent<HTMLInputElement>) => {
         const target = e.target as HTMLInputElement;
         setSearchValue(() => target.value);
     }
 
     const handleSubmit = () => {
-        /*const fetch = async () => {
-            await trigger({ location: searchValue });
-            if (data) setWeather(data);
-        }
-
-        fetch();*/
         setSubmitClick(!submitClick);
     }
 
     const handleLocationClick = () => {
-        // getGeolocation();
         setLocationClick(!locationClick);
     }
 
